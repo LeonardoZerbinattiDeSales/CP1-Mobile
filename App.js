88ec7679d39f840c8a2a6366a851d47cde84b821
LeonardoZerbinattiDeSales <leonardozerbinattidesales@gmail.com>
2026-03-20 15:15:48 -0300

    Cria estrutura inicial da tela

--- App.js
+++ App.js
@@ -1,20 +1,57 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  Alert,
+  TouchableOpacity,
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.card}>
+        <Text style={styles.titulo}>Formulário de Cadastro</Text>
+        <Text style={styles.textoTopo}>
+          Estrutura inicial do aplicativo.
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#000000',
     justifyContent: 'center',
+    padding: 20,
+  },
+  card: {
+    backgroundColor: '#111111',
+    borderRadius: 20,
+    padding: 20,
+    borderWidth: 1,
+    borderColor: '#00F5FF',
+  },
+  titulo: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#FFFFFF',
+    textShadowColor: '#00F5FF',
+    textShadowOffset: { width: 0, height: 0 },
+    textShadowRadius: 12,
+    marginBottom: 10,
+  },
+  textoTopo: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#D1D5DB',
   },
 });
